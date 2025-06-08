ae498032db1ed436f755d1eb59fdffc7674c8863
Frank Grinaert <frank.grinaert@wfp.org>
2025-06-08 18:28:54 +0200

    Update color-palette.tsx

--- color-palette.tsx
+++ color-palette.tsx
@@ -448,7 +448,7 @@ export default function ColorPalette() {
     // "brown",
     "neutral"
   ]
-  const steps = [95, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+  const steps = [0, 95, 90, 80, 70, 60, 50, 40, 30, 20, 10]
 
   const copyToClipboard = (text: string) => {
     navigator.clipboard.writeText(text)
